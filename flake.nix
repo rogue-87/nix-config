ca581c28314a47fda319f44818f48f3713098e79
Rogue <rogue87@tuta.io>
2025-04-15 00:34:56 +0200

    give myself headache for no reason
    perhaps I'll give home-manager another shot
    I'll stick with stow for now

--- flake.nix
+++ flake.nix
@@ -1,29 +1,23 @@
 {
-  description = "Nixos config flake";
+  description = "my nix/OS flake :D";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
-    # use "nixos", or your hostname as the name of the configuration
-    # it's a better practice than "default" shown in the video
+  outputs =
+    { self, nixpkgs, ... }@inputs:
     let
+      lib = nixpkgs.lib;
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
     in
     {
       nixosConfigurations = {
-        laptop = nixpkgs.lib.nixosSystem {
-          specialArgs = {inherit inputs system;};
+        laptop = lib.nixosSystem {
+          inherit system;
+          specialArgs = { inherit inputs; };
           modules = [
             ./hosts/laptop/configuration.nix
-            inputs.home-manager.nixosModules.default
           ];
         };
       };
--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -1,30 +1,20 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
-{ config, pkgs, inputs,... }:
-
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      inputs.home-manager.nixosModules.default
-    ];
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
+{
+  imports = [ ./hardware-configuration.nix ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  nix.settings.experimental-features = ["nix-command" "flakes"];
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 
   # Set your time zone.
   time.timeZone = "Africa/Tripoli";
@@ -44,9 +34,122 @@
     LC_TIME = "en_AG";
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
+  users.users.rogue = {
+    isNormalUser = true;
+    description = "Rogue";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+    packages = with pkgs; [
+      # general stuff
+      discord
+      youtube-music
+			syncthing
+			syncthingtray-minimal
+			# heroic
+
+      fish # fav shell
+      neovim # fav code editor
+      neovide # neovim gui frontend
+      tree-sitter
+
+      # langs
+      lua
+      nodejs # lame js runtime
+      deno # superior js runtime
+      python3
+      jdk
+      clang
+      rustup
+      taplo
+
+      # language servers
+      nixd # for nix
+
+      bash-language-server
+      fish-lsp
+
+      lua-language-server
+      pyright
+      jdt-language-server
+
+      svelte-language-server
+      vscode-langservers-extracted # html, css, json, markdown?
+
+      # for taking notes
+      marksman
+      tinymist
+
+      # formatters
+      prettierd
+      nixfmt-rfc-style
+      ruff
+      shfmt
+      stylua
+
+      # linters
+      selene
+
+      # package managers
+      luarocks
+
+      # cli/tui tools
+      btop
+      gh
+      gitoxide
+      zoxide
+      just
+      lazygit
+      stow
+      zellij
+      docker
+
+      # terminal emulators
+      kitty
+      rio
+
+      # fonts
+      nerd-fonts.jetbrains-mono
+      nerd-fonts.symbols-only
+    ];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    kdePackages.kate
+    kdePackages.plasma-browser-integration
+    chromium
+    git
+    fd
+    gcc
+    librewolf
+    ripgrep
+    speedtest-rs
+    tree
+    wget
+    wl-clipboard
+  ];
+
+  # **services**
+  # services.openssh.enable = true;
+
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+
+    jack.enable = false;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  services.xserver.enable = false;
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
@@ -61,87 +164,51 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
+  # **hardware**
   hardware.bluetooth.enable = true;
 
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.rogue = {
-    isNormalUser = true;
-    description = "Rogue";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      kdePackages.kate
-      neovim
-      rio
-      kitty
-      discord
-      librewolf
-      youtube-music
-      gh
-    ];
-  };
-
-  home-manager = {
-    # also pass inputs to home-manager modules
-    extraSpecialArgs = { inherit inputs; };
-    users = {
-    	"rogue" = import ./home.nix;
-    };
-  };
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    wget
-  ];
-
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
+  # **network**
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # the green devil that is nvidia
+  hardware.graphics = {
+    enable = true;
+  };
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = true;
+    nvidiaSettings = true;
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  # WARN: read "man configuration.nix" or "https://nixos.org/nixos/options.html"  before changing
+  system.stateVersion = "24.11";
 }
